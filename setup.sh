8f2dbe4c1e08892f7fd0ac7198641864e02c0875
Aretas Paulauskas <aretas.pau@gmail.com>
2020-01-11 16:47:22 +0200

    setup: add minimal setup.sh script

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+setup_ohmyzsh_theme()
+{
+	themes_dir=${ZSH_CUSTOM}/themes
+	powerlevel10k_dir=${ZSH_CUSTOM}/themes/powerlevel10k
+
+	if [ ! -d "${themes_dir}" ]; then
+		return
+	fi
+
+	if [ -d "${powerlevel10k_dir}" ]; then
+		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${powerlevel10k_dir}"
+	fi
+}
+
+setup_ohmyzsh()
+{
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+setup_zsh()
+{
+	setup_ohmyzsh
+	setup_ohmyzsh_theme
+}
+
+
+# used apps
+apps=(git curl)
