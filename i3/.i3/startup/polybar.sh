66336d03cced2b5e330af2f4640944fa3cd87556
Aretas Paulauskas <aretas.pau@gmail.com>
2020-01-18 18:13:19 +0200

    i3: run polybar for each monitor

--- i3/.i3/startup/polybar.sh
+++ i3/.i3/startup/polybar.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar1 >>/tmp/polybar1.log 2>&1 &
-polybar bar2 >>/tmp/polybar2.log 2>&1 &
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+	MONITOR=$m polybar --reload top -r&
+done
+
